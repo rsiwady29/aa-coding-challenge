5583ea5a44a47038701a318bd24dcc3b5ecc9260
Richard Siwady <rsiwady29@gmail.com>
2015-09-13 11:54:15 -0600

    Updating server

--- main.py
+++ main.py
@@ -13,4 +13,5 @@ class Main(Resource):
 api.add_resource(Main, '/')
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
