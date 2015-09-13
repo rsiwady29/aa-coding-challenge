7faeb3943190b452d03d3c2231f0c959803fc396
Richard Siwady <rsiwady29@gmail.com>
2015-09-13 13:40:53 -0600

    added word-splitter

---
+++ utilities/word-splitter.py
@@ -0,0 +1,21 @@
+class WordSplitter:
+    ## Naive Approach
+    def splitwords(self, englishWordList, wordList):
+        newList = []
+        for word in wordList:
+            i = 1
+            curWord = word[0]
+            while i < len(word):
+                if (curWord in englishWordList):
+                    newList += [curWord]
+                    curWord = ""
+                elif i == len(word) -1:
+                    curWord += word[i]
+                    newList += [curWord]
+                curWord += word[i]
+                i += 1
+        return newList
+
+#Testcases
+ws = WordSplitter()
+assert ws.splitwords(["a", "cat","cats", "two", "dogs"], ['acat', "twodogs"]  ) == ["a", "cat", "two", "dogs"]
